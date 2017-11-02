c55a95dd7e5eb7bb0ab20b5c25867374ed5a2ad4
Wence Criado <wence74@gmail.com>
2017-11-02 20:02:34 +0100

    Trabajando con DB remota

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   $user = 'root';
+   $dbpassword = 'root';
+   $db = 'incidencias_FOC';
+   $host = 'localhost';
+   $port = 8889; 
+?>
--- index.php
+++ index.php
@@ -1,26 +1,47 @@
 <?php
+include("config.php");
 session_start();
 // NO HAY VARIABLE SESSION
 if (!isset($_SESSION['usuario'])){  
+  
   // NO HAY POST
   if(!isset ($_POST['login'])){
- 
+    
   }
   // SI HAY POST
-  else {  
-    if($_POST['password'] == '1234'){
-       // Variable SESSION usuario
-       $_SESSION['usuario'] = 'admin';      
-      header('Location: /perfiles/admin.php');      
+  else {       
+
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password']; 
+    
+    // Creo conexión
+    $conn = new mysqli("$host:$port", $user, $dbpassword, $db);
+    // Compruebo si hay errores
+    if ($conn->connect_error) {
+        die("Problema de conexión: " . $conn->connect_error);
+    } 
+    
+    
+    $sql = "SELECT rol FROM users WHERE usuario = '$usuario' and password = '$password'";
+    $result = $conn->query($sql);
+    // Si el contador de la consulta devuelve 1, es que hay usuario y password en la DB
+    if ($result->num_rows > 0) {
+        // Inserto el resultado en $row
+        while($row = $result->fetch_assoc()) {
+
+            if($row["rol"] == 1){
+                $_SESSION['usuario'] = 'admin';
+                header('Location: /perfiles/admin.php'); 
+            }
+            else{
+                $_SESSION['usuario'] = 'usuario';
+                header('Location: /perfiles/usuario.php');
+            }
+        }
+    } else {
+        
     }
-    elseif($_POST['password'] == '1'){
-      // Variable SESSION usuario  
-      $_SESSION['usuario'] = 'usuario';          
-      header('Location: /perfiles/usuario.php');   
-    } 
-    else {
-      // Variable SESSION usuario        
-    } 
+    $conn->close(); 
   }
 ?>
 <!DOCTYPE html>
@@ -37,7 +58,7 @@ if (!isset($_SESSION['usuario'])){
   <body>
     <div class="container">
 
-      <form class="form-signin" name="form1" method="post" action="">
+      <form class="form-signin" name="form1" method="post" action="/">
         <h2 class="form-signin-heading">Incidencias | FOC</h2>
         <input name="usuario" id="u_nombre" type="text" class="form-control" placeholder="Usuario" autofocus>
         <input name="password" id="u_pass" type="password" class="form-control" placeholder="Password">
@@ -63,12 +84,13 @@ if (!isset($_SESSION['usuario'])){
   // SI HAY VARIABLE SESSION
   }
   else{
+    
     if(($_SESSION['usuario'] == 'admin')){
         
          header('Location: /perfiles/admin.php');     
      }
      else if(($_SESSION['usuario'] == 'usuario')){       
-        
+        echo 'soy user';
          header('Location: /perfiles/usuario.php');
      }
      else{        
